25610db565ba528fb623dfbf345d5d8d2c698738
sudais122 <khansb17798@gmail.com>
2026-02-25 20:38:45 +0500

    lecture 3 added

---
+++ vite-project/src/Apps.jsx
@@ -0,0 +1,12 @@
+function Code(){
+    return (
+    <h5>helo Decaloper Sudais</h5>
+    )
+}
+
+function Math(){
+    return(
+        <h5>2+2=4</h5>
+    )
+}
+export {Code,Math};
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Code, Math } from './Apps.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <Code />
+    <Math />
+  </>
 )
