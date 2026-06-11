bd02f9b1da896cc9b66b45216386d6f2c0011d1a
sudais122 <khansb17798@gmail.com>
2026-06-12 00:29:31 +0500

    Start

--- Bg-Changer/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'   
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Main from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <Main />
-)
